70593ddf7ed1eec134b4b1e949728f0689b4cac4
Smulskyi Ihor <smulia755@gmail.com>
2018-12-17 17:15:45 +0000

    works variant

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 const a = parseFloat(prompt('Enter A'));
 const b = parseFloat(prompt('Enter B'));
 const c = parseFloat(prompt('Enter C'));
+const result = solveQuadrEq(a,b,c);
+alert(result);
 
 function solveQuadrEq(a,b,c){
     const d = calcDiscr(a,b,c);
-    if (d>0) {
-         const x1 =((-b)+(sqrt(d))/(2*a)
-         const x2 =((-b)-(sqrt(d))/(2*a)
-         return('x1= '+ x1 +',x2= '+ x2)
+    if (d=>0) {
+         const x1 =-b+Math.sqrt(d)/(2*a);
+         const x2 =-b-Math.sqrt(d)/(2*a);
+         return('x1= '+ x1 +',x2= '+ x2);
     }
     else{
         alert('D<0')
     }
     function calcDiscr(a,b,c){
-        return ((b*b)-4*a*c);
+        return b*b-4*a*c;
     }
 }

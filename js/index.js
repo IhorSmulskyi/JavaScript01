bcb40f64640a1d93dc12e3aa15586fb32f668d49
Smulskyi Ihor <smulia755@gmail.com>
2018-12-17 16:55:43 +0000

    first step

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const a = parseFloat(prompt('Enter A'));
+const b = parseFloat(prompt('Enter B'));
+const c = parseFloat(prompt('Enter C'));
+
+function solveQuadrEq(a,b,c){
+    const d = calcDiscr(a,b,c);
+    if (d>0) {
+         const x1 =((-b)+(sqrt(d))/(2*a)
+         const x2 =((-b)-(sqrt(d))/(2*a)
+         return('x1= '+ x1 +',x2= '+ x2)
+    }
+    else{
+        alert('D<0')
+    }
+    function calcDiscr(a,b,c){
+        return ((b*b)-4*a*c);
+    }
+}

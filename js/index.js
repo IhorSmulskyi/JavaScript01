6cf9480b853c1d38297c09284154457fd2324ffc
Smulskyi Ihor <smulia755@gmail.com>
2018-12-17 17:20:05 +0000

    last version

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ alert(result);
 function solveQuadrEq(a,b,c){
     const d = calcDiscr(a,b,c);
     if (d=>0) {
-         const x1 =-b+Math.sqrt(d)/(2*a);
-         const x2 =-b-Math.sqrt(d)/(2*a);
+         const x1 =(-b+Math.sqrt(d))/(2*a);
+         const x2 =(-b-Math.sqrt(d))/(2*a);
          return('x1= '+ x1 +',x2= '+ x2);
     }
     else{
